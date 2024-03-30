18dd34f89d115c0868ff2b123b8f703cdd801902
Arconix-Orsheart <captainalex011@gmail.com>
2024-03-30 16:02:36 +0200

    nn

--- src/org/tuc/Lists/DList.java
+++ src/org/tuc/Lists/DList.java
@@ -1,5 +1,32 @@
 package org.tuc.Lists;
 
-public class DList {
+import org.tuc.Element;
+import org.tuc.List;
+
+public class DList implements List {
+
+    static class Node {
+        Element data;
+        Node next;
+
+        Node(Element elem) {
+            data = elem;
+        }
+    }
+
+    @Override
+    public boolean insert(Element element) {
+        return true;
+    }
+
+    @Override
+    public boolean delete(int key) {
+        return true;
+    }
+
+    @Override
+    public Element search(int key) {
+        return null;
+    }
 
 }

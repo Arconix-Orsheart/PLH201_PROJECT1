8f6823910cda6d0847e7dd7125ef137a796edae8
Arconix-Orsheart <captainalex011@gmail.com>
2024-03-30 15:14:00 +0200

    second

--- src/org/tuc/Element.java
+++ src/org/tuc/Element.java
@@ -1,6 +1,13 @@
 package org.tuc;
 
+/**
+ * Data stored in the data structure with int keys
+ */
 public interface Element {
-
+    /**
+     * Returns the key of this element
+     * 
+     * @return the key of the element
+     */
     public int getKey();
 }
